b8f82ab16eeb8a5783b1f5091afd9943129f0f4e
sethcrist <sethcrist50@gmail.com>
2024-04-10 17:12:13 -0700

    added 2 more apartments to the seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ apartments = [
     unit: '5A',
     city: 'NY',
     state: 'NY',
-    square_footage: '1000',
+    square_footage: '1,000',
     price: '2000',
     bedrooms: 1,
     bathrooms: 1.0,
@@ -21,12 +21,40 @@ apartments = [
     unit: 'PH',
     city: 'NY',
     state: 'NY',
-    square_footage: '17545',
-    price: '196,000,000',
+    square_footage: '17,545',
+    price: '20,000',
     bedrooms: 7,
     bathrooms: 11.0,
     pets: 'pets allowed',
     image: 'https://www.zillow.com/homedetails/217-W-57th-St-PH-New-York-NY-10019/2067603580_zpid/',
+    user_id: user1
+  },
+
+  {
+    street: '114 Calle Catalina',
+    unit: 'PL',
+    city: 'HTX',
+    state: 'TX',
+    square_footage: '4,954',
+    price: '9,900',
+    bedrooms: 4,
+    bathrooms: 4,
+    pets: 'pets allowed',
+    image: 'https://www.zillow.com/homedetails/114-Calle-Catalina-Pl-Houston-TX-77007/123237523_zpid/',
+    user_id: user2
+  },
+
+  {
+    street: '622 Pink Azalea',
+    unit: 'TRL',
+    city: 'HTX',
+    state: 'TX',
+    square_footage: '4,722',
+    price: '9,450',
+    bedrooms: 4,
+    bathrooms: 3.5,
+    pets: 'pets allowed',
+    image: 'https://www.zillow.com/homedetails/622-Pink-Azalea-Trl-Houston-TX-77079/118162425_zpid/',
     user_id: user2
   }
 ]
